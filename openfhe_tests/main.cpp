c51fe11315d90649e6f54f28ecfbe4c87d8bab8e
AnasIshfaque <aosmani203004@bscse.uiu.ac.bd>
2024-05-05 17:54:11 +0600

    added delay before and after main code

--- openfhe_tests/main.cpp
+++ openfhe_tests/main.cpp
@@ -24,12 +24,12 @@ int main() {
     std::system("ls");
     // starting the device tracking
     std::thread bash_thread([](){
-        std::system("../laptopcheck.sh");
+        std::system("../rasp_check.sh");
     });
 
     bash_thread.detach();
 
-    // std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::this_thread::sleep_for(std::chrono::seconds(3));
 
     // Get the current time
     auto start_time = std::chrono::system_clock::now();
@@ -147,18 +147,18 @@ int main() {
     plaintextRot3->SetLength(vectorOfInts1.size());
     plaintextRot4->SetLength(vectorOfInts1.size());
 
-    std::cout << "Plaintext #1: " << plaintext1 << std::endl;
-    std::cout << "Plaintext #2: " << plaintext2 << std::endl;
-    std::cout << "Plaintext #3: " << plaintext3 << std::endl;
+    // std::cout << "Plaintext #1: " << plaintext1 << std::endl;
+    // std::cout << "Plaintext #2: " << plaintext2 << std::endl;
+    // std::cout << "Plaintext #3: " << plaintext3 << std::endl;
 
     // Output results
-    std::cout << "\nResults of homomorphic computations" << std::endl;
-    std::cout << "#1 + #2 + #3: " << plaintextAddResult << std::endl;
-    std::cout << "#1 * #2 * #3: " << plaintextMultResult << std::endl;
-    std::cout << "Left rotation of #1 by 1: " << plaintextRot1 << std::endl;
-    std::cout << "Left rotation of #1 by 2: " << plaintextRot2 << std::endl;
-    std::cout << "Right rotation of #1 by 1: " << plaintextRot3 << std::endl;
-    std::cout << "Right rotation of #1 by 2: " << plaintextRot4 << std::endl;
+    // std::cout << "\nResults of homomorphic computations" << std::endl;
+    // std::cout << "#1 + #2 + #3: " << plaintextAddResult << std::endl;
+    // std::cout << "#1 * #2 * #3: " << plaintextMultResult << std::endl;
+    // std::cout << "Left rotation of #1 by 1: " << plaintextRot1 << std::endl;
+    // std::cout << "Left rotation of #1 by 2: " << plaintextRot2 << std::endl;
+    // std::cout << "Right rotation of #1 by 1: " << plaintextRot3 << std::endl;
+    // std::cout << "Right rotation of #1 by 2: " << plaintextRot4 << std::endl;
 
     // Get the end time
     auto end_time = std::chrono::system_clock::now();
@@ -182,10 +182,10 @@ int main() {
     // Print the size of cryptoContext
     std::cout << "Size of cryptoContext: " << sizeof(*cryptoContext) << " bytes" << std::endl;
 
-    // std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::this_thread::sleep_for(std::chrono::seconds(3));
 
     // Terminate the bash script process
-    terminateProcess("../laptopcheck.sh");
+    terminateProcess("../rasp_check.sh");
 
     return 0;
 }
--- openfhe_tests/rasp_check.sh
+++ openfhe_tests/rasp_check.sh
@@ -5,12 +5,8 @@ CSV_FILE="../system_metrics.csv"
 # Function to get CPU usage
 get_cpu_usage() {
     # Extract CPU usage from powertop_output.csv
-    cpu_usage=$(awk -F';' '/Device Power Report/ {getline; getline; getline; print $1 }' ../powertop_output.csv)
+    cpu_usage=$(awk -F';' '/Device Power Report/ {getline; getline; getline; print $1 }' powertop_output.csv)
     echo "$cpu_usage"
-}
-
-get_top_cpu_usage() {
-    top -bn1 | grep "Cpu(s)" | sed "s/., *\([0-9.]\)%* id.*/\1/" | awk '{print 100 - $1}'
 }
 
 # Function to get RAM usage
@@ -18,10 +14,6 @@ get_ram_usage() {
     free -m | awk '/Mem:/ {print $3}'
 }
 
-get_vcgencmd_ram() {
-	ram_used=$(vcgencmd get_mem arm | awk -F'=' '{print $2}')
-	echo "$ram_used"
-}
 # Function to get CPU temperature
 get_cpu_temperature() {
     # Check if vcgencmd utility is available
@@ -37,14 +29,16 @@ get_cpu_temperature() {
 
 get_power_draw() {
     # Extract power draw from powertop_output.csv
-    power_draw=$(awk -F';' '/Device Power Report/ {getline; getline; getline; print $3 }' ../powertop_output.csv)
+    power_draw=$(awk -F';' '/Device Power Report/ {getline; getline; getline; print $3 }' powertop_output.csv)
     echo "$power_draw"
 }
 
 # Function to remove temporary powertop_output.csv
 remove_temp_file() {
-    rm -f ../powertop_output.csv
+    rm -f powertop_output.csv
 }
+
+echo "timestamp,cpu_usage,ram_usage,get_cpu_temperature,power_draw" >> "$CSV_FILE"
 
 # Append system metrics to CSV file
 append_metrics_to_csv() {
@@ -57,15 +51,16 @@ append_metrics_to_csv() {
     power_draw=$(get_power_draw)
     
     # Append the metrics to the CSV file
-    echo "$timestamp,$cpu_usage,$ram_usage,$cpu_temperature,$power_draw" >> "$CSV_FILE"
+    echo "$timestamp,$cpu_usage,$ram_usage,$get_cpu_temperature,$power_draw" >> "$CSV_FILE"
     # echo "$timestamp,$ram_usage" >> "$CSV_FILE"
 }
 
+counter=0
 
-while true; do
+while [ $counter -lt 100 ]; do
 
     # Run powertop to generate output and extract metrics
-    sudo powertop --time=0.001 --csv=./openfhe_tests/powertop_output.csv &> /dev/null
+    sudo powertop --time=0.001 --csv=powertop_output.csv &> /dev/null
 
     # Check if powertop command was successful
     if [ $? -eq 0 ]; then
@@ -76,6 +71,8 @@ while true; do
     else
         echo "N/A (powertop failed to run)"
     fi
+
+    ((counter++))
 
     # Adjust sleep duration as needed
     sleep 0.001
